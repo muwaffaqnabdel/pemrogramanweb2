130ceb414b2a16b94bee872161c388f0614d294f
abdellmuwaffaq <abdellmuwaffaq@gmail.com>
2025-03-15 02:53:34 +0700

    pertemuan1 instalasi dan pengenalan laravel

---
+++ halo.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halo Page</title>
+    
+    <!-- Hubungkan file CSS -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="header">
+        <h1 class="custom-text">Selamat Datang di Tokopedia E-commerce</h1>
+    </div>
+
+    
+
+    <div class="isiawal">
+        <img src="" alt="tokopedia.jpg">
+        <h1>Tokopedia</h1>
+    </div>
+
+
+    <div class="deskripsi">
+        <h2>Tokopedia resmi diluncurkan ke publik pada 17 Agustus 2009 di bawah naungan PT Tokopedia yang didirikan oleh William Tanuwijaya dan Leontinus Alpha Edison pada 6 Februari 2009. Sejak resmi diluncurkan, PT Tokopedia berhasil menjadi salah satu perusahaan internet Indonesia dengan pertumbuhan yang sangat pesat.[8]
+Pada tahun 2016, Tokopedia menghadirkan produk teknologi finansial. Produk fintech Tokopedia terdiri dari dompet digital, investasi terjangkau, kredit modal bisnis, kartu kredit virtual, produk proteksi, skoring kredit berdasarkan data untuk produk pinjaman, investasi, serta layanan keuangan lainnya[9]
+Pada tahun 2017, Tokopedia meluncurkan produk Deals untuk membantu masyarakat Indonesia mendapatkan penawaran terbaik dari delapan kategori, termasuk Travel dan Activity. Produk ini dimaksudkan untuk membantu bisnis offline melebarkan sayap mereka secara online melalui Tokopedia</h2>
+</body>
+</html>
